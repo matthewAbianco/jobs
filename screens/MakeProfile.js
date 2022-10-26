a17b314251f2d45ec1f9ca87be4aa6ce8e04f90c
unknown <matthewbianco@hotmail.com>
2022-10-26 12:49:52 -0400

    fc

--- screens/MakeProfile.js
+++ screens/MakeProfile.js
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, KeyboardAvoidingView } from "react-native"
+import React from "react"
 
 const MakeProfile = () => {
-    return (
+  return (
+    <KeyboardAvoidingView>
+      <SafeAreaView>
         <View>
-            <Text>MakeProfile</Text>
+          <Text>MakeProfile</Text>
         </View>
-    )
+        {/* /Information to edit on user profile */}
+        {/* cover photo */}
+        {/* user display photo */}
+        {/* biography of user/business */}
+        {/* specialities */}
+        {/* section packages and services offerd */}
+        {/* photos and videos of work */}
+        {/* logos of companies worked for */}
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  )
 }
 
 export default MakeProfile
--- screens/Reviews.js
+++ screens/Reviews.js
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native"
 import React from "react"
 
-const Services = () => {
+const Reviews = () => {
   return (
     <View>
-      <Text>Services</Text>
+      <Text>Reviews</Text>
     </View>
   )
 }
 
-export default Services
+export default Reviews
